7d0d0a8d81de929a4e7278d58c1c199c65d46fe9
Christopher-Robin <jecebbi@gmail.com>
2020-02-10 00:41:04 +1100

    Fixed KeyManager and added Mouse Manager

--- Ohrogon/Mesh.h
+++ Ohrogon/Mesh.h
@@ -1,7 +1,14 @@
 #pragma once
-
+#include <atyp_Vector3.h>
 
 class Mesh{
+	int ItemCount;
 
+	Vector3* Verticies;
+	Vector3* Normals;
+	Vector3* Colors;
+	Vector2* UVs;
+
+	int* indicies;
+	int indexLength;
 };
-
---
+++ Ohrogon/MouseManager.cpp
@@ -0,0 +1,9 @@
+#include "MouseManager.h"
+
+MouseManager* MouseManager::Instance = nullptr;
+
+//
+//float MouseManager::x = 0.0f;
+//float MouseManager::y = 0.0f;
+//float MouseManager::dx = 0.0f;
+//float MouseManager::dy = 0.0f;
