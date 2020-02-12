3b3a8713fef94fe30f7b9bb829f409e2c145fe1e
Christopher-Robin <jecebbi@gmail.com>
2020-02-12 17:32:34 +1100

    Fixed Mesh Binding and Verted Attributes

--- Ohrogon/Shaders/VertShader.glsl
+++ Ohrogon/Shaders/VertShader.glsl
@@ -1,10 +1,13 @@
 #version 450
 
 layout(location = 0) in vec3 VertPos;
+layout(location = 1) in vec3 VertNormal;
 
  uniform mat4 MVPMatrix;
 // uniform mat4 ModelMatrix;
+out vec3 Color;
 
 void main(){
 	gl_Position = MVPMatrix * vec4(VertPos, 1);
+	Color = VertNormal;
 }
