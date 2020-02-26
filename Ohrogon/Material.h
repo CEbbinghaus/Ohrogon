8d2b781fdf29518f170512d475267d4f237839ac
Christopher-Robin <jecebbi@gmail.com>
2020-02-26 12:59:33 +1100

    Changed to Vector4

--- Ohrogon/Material.h
+++ Ohrogon/Material.h
@@ -1,37 +1,41 @@
 #pragma once
 #include <atyp_Vector3.h>
+#include <atyp_Vector4.h>
 #include "gl_core_4_5.h"
 
 using uint = unsigned int;
 
-class Material{
-public:
-    Vector3 Ka; // ambient material colour
-    Vector3 Kd; // diffuse material colour
-    Vector3 Ks; // specular material colour
+struct MaterialProperties{
+    Vector4 Ka; // ambient material colour
+    Vector4 Kd; // diffuse material colour
+    Vector4 Ks; // specular material colour
+    Vector4 Ia; // ambient light colour
+    Vector4 Id; // diffuse light colour
+    Vector4 Is; // specular light colour
+    Vector4 LightDirection;
     float specularPower; // material specular power
-    Vector3 Ia; // ambient light colour
-    Vector3 Id; // diffuse light colour
-    Vector3 Is; // specular light colour
-    Vector3 LightDirection;
+};
 
+class Material : MaterialProperties{
+public:
     uint programmid;
+    uint bindingPoint = 1, buffer, blockIndex;
     Material(uint programm){
         programmid = programm;
 
-        Ka = Vector3::one() * 0.1;
-        Kd = Vector3::one() * 0.2;
-        Ks = Vector3::one() * 0.1;
-        Ia = Vector3::one() * 0.25f;
-        Id = Vector3::one() - Vector3::forward();
-        Is = Vector3::one() - Vector3::forward();
+        Ka = Vector4(0.5f, .0f, 0.5f);
+        Kd = Vector4::one();
+        Ks = Vector4::one();
+        Ia = Vector4::one();
+        Id = Vector4::one() - Vector4::forward();
+        Is = Vector4::one() - Vector4::forward();
 
-        LightDirection = Vector3::up();
-        specularPower = .001f;
-    }
-    
-    void Bind(){
-        uint bindingPoint = 1, buffer, blockIndex;
+        LightDirection = Vector4::one();
+
+        specularPower = 50.0f;
+
+        LightDirection.Print();
+
         blockIndex = glGetUniformBlockIndex(programmid, "Material");
         glUniformBlockBinding(programmid, blockIndex, bindingPoint);
 
@@ -43,9 +47,11 @@ public:
             #endif
 
         glGenBuffers(1, &buffer);
+    }
+    
+    void Bind(){
         glBindBuffer(GL_UNIFORM_BUFFER, buffer);
-
-        glBufferData(GL_UNIFORM_BUFFER, sizeof(Material), this, GL_DYNAMIC_DRAW);
+        glBufferData(GL_UNIFORM_BUFFER, sizeof(MaterialProperties), &Ka, GL_STATIC_DRAW);
         glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, buffer);
     }
 };
