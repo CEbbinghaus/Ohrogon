ac471d111684adea14a81d94898b9ff4da712f4b
Christopher-Robin <jecebbi@gmail.com>
2020-03-23 15:51:41 +1100

    Added Collider and Rigidbody Component

---
+++ Ohrogon/Collider.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Component.h"
+
+class Collider : Component{
+
+};
+
+class ColliderManager: ComponentManager{
+	friend Collider;
+public:
+
+	void Setup(){
+		
+	}
+	void Execute(){
+
+	}
+	void Register(){
+
+	}
+	void Destroy(){
+
+	}
+
+};
